1ea283b65ac2cee1dbc1f1d933052b32799563d1
Yasuhiro Usutani <y_usutani@mac.com>
2021-03-19 18:56:13 +0900

    Mod Account::Registration

--- app/models/account/registration.rb
+++ app/models/account/registration.rb
@@ -1,2 +1,10 @@
 class Account::Registration < ApplicationRecord
-end
+  validates :email, presence: true
+  validate :email_of_account_is_unique
+  validates :terms_of_service, acceptance: true
+
+  private
+    def email_of_account_is_unique
+      errors.add(:email, 'は既に使用されています。') if Account.find_by(email: email)
+    end
+end 
