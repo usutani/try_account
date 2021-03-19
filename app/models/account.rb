5a6c555f8579566a3a5be727fbb9e9e5e87e39c9
Yasuhiro Usutani <y_usutani@mac.com>
2021-03-19 19:05:34 +0900

    Mod Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,7 @@
 class Account < ApplicationRecord
   has_secure_password
-end
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 8..30 },
+                       format: { with: /\A[a-z0-9]+\z/i,
+                                 message: 'は半角英数とハイフン（-）のみお使いください。' }
+ end
