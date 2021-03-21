ee1a13e25b066d87baa1f54a2d649a2275455ea4
Yasuhiro Usutani <y_usutani@mac.com>
2021-03-21 11:51:32 +0900

    resources :freezes, only: :index

--- app/controllers/accounts/freezes_controller.rb
+++ app/controllers/accounts/freezes_controller.rb
@@ -1,4 +1,8 @@
 class Accounts::FreezesController < ApplicationController
+  def index
+    @accounts = Account.joins(:freeze).where.not(freeze: nil).order(:id)
+  end
+
   def create
     Account.find(params[:account_id]).create_freeze
     redirect_to accounts_url
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :account do
     resources :registrations, only: %i[show new create]
   end
+  namespace :accounts do
+    resources :freezes, only: :index
+  end
   resources :accounts, only: %i[index show new create] do
     resource :freeze, only: %i[create destroy], module: 'accounts'
   end
